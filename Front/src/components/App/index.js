af75c80d9ec5f1a04f529ab929d8613b919e45b5
jonathan fuzeau <jfuzeau@hotmail.fr>
2021-03-18 13:54:30 +0100

    mise en place l'architecture

--- Front/src/components/App/index.js
+++ Front/src/components/App/index.js
@@ -2,14 +2,12 @@
 import React from 'react';
 
 // == Import
-import reactLogo from './react-logo.svg';
-import './styles.css';
+import 'src/styles/index.scss';
 
 // == Composant
 const App = () => (
   <div className="app">
-    <img src={reactLogo} alt="react logo" />
-    <h1>Composant : App</h1>
+    <h1>App</h1>
   </div>
 );
 
