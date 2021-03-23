bf9899caeaf9730c4810c043cc414adf9fac404a
jonathan fuzeau <jfuzeau@hotmail.fr>
2021-03-23 20:07:58 +0100

    change footer display and add links to social networks

--- Front/src/components/Footer/index.js
+++ Front/src/components/Footer/index.js
@@ -1,24 +1,39 @@
+// Import npm
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Footer = () => {
-  return (
-    <footer className="Footer">
-    <p>E-PAKO Copyright &#169; 2021. Tous droits réservés.</p>
-      <ul>
-        <li>
-          <Link to="/apropos">
-            A propos
-          </Link>
-        </li>
-        <li>
-          <Link to="/mentionslegales">
-            Mentions légales
-          </Link>
-        </li>
-      </ul>
-    </footer>
-  );
-};
+// Import local
+import facebook from 'src/assets/images/socials/facebook.svg';
+import twitter from 'src/assets/images/socials/twitter.svg';
+
+const Footer = () => (
+  <footer className="Footer">
+    <p className="Footer__copyright">
+      E-PAKO Copyright &#169; 2021. Tous droits réservés.
+    </p>
+    <ul className="Footer__links">
+      <li>
+        <Link to="/apropos">
+          A propos
+        </Link>
+      </li>
+      <li>
+        <Link to="/mentionslegales">
+          Mentions légales
+        </Link>
+      </li>
+      <li className="Footer__links-socials">
+        <div>
+          <a href="https://facebook.com">
+            <img src={facebook} alt="facebook-icon" />
+          </a>
+          <a href="https://twitter.com">
+            <img src={twitter} alt="twitter-icon" />
+          </a>
+        </div>
+      </li>
+    </ul>
+  </footer>
+);
 
 export default Footer;
