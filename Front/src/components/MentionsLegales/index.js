da3ce30defdd97e170ffa9226d47cc3baca6c7de
Alex-Oz-157aws <maspimbya@gmail.com>
2021-03-23 21:26:07 +0100

    mentions-legales-ok

--- Front/src/components/MentionsLegales/index.js
+++ Front/src/components/MentionsLegales/index.js
@@ -4,7 +4,8 @@ const MentionsLegales = () => {
   return (
     <section className="Mentions-legales">
       <article className="Mentions-legales__article">
-        <h1 className="Mentions-legales__article-title">Un titre</h1>
+        <h1 className="Mentions-legales__article-title">Repellendus omnis</h1>
+        <h2 className="Mentions-legales__article-title--2">Sapiente ipsa repudiandae eveniet</h2>
         <p className="Mentions-legales__article-content">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus omnis nisi quaerat doloribus, vitae laudantium error hic eos tenetur similique ipsum consectetur voluptatem ipsam exercitationem corrupti ea iusto culpa odio!
           Et ab recusandae possimus nobis dolor? Iste, totam. Est fugit ab atque itaque minus, omnis nam quos cum. Necessitatibus velit eaque harum impedit saepe ex maiores! Aperiam amet doloribus voluptatem.
@@ -15,7 +16,8 @@ const MentionsLegales = () => {
         </p>
       </article>
       <article className="Mentions-legales__article">
-        <h1 className="Mentions-legales__article-title">Un titre</h1>
+        <h1 className="Mentions-legales__article-title">Repellendus omnis</h1>
+        <h2 className="Mentions-legales__article-title--2">Sapiente ipsa repudiandae eveniet</h2>
         <p className="Mentions-legales__article-content">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus omnis nisi quaerat doloribus, vitae laudantium error hic eos tenetur similique ipsum consectetur voluptatem ipsam exercitationem corrupti ea iusto culpa odio!
           Et ab recusandae possimus nobis dolor? Iste, totam. Est fugit ab atque itaque minus, omnis nam quos cum. Necessitatibus velit eaque harum impedit saepe ex maiores! Aperiam amet doloribus voluptatem.
@@ -26,7 +28,8 @@ const MentionsLegales = () => {
         </p>
       </article>
       <article className="Mentions-legales__article">
-        <h1 className="Mentions-legales__article-title">Un titre</h1>
+        <h1 className="Mentions-legales__article-title">Repellendus omnis</h1>
+        <h2 className="Mentions-legales__article-title--2">Sapiente ipsa repudiandae eveniet</h2>
         <p className="Mentions-legales__article-content">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus omnis nisi quaerat doloribus, vitae laudantium error hic eos tenetur similique ipsum consectetur voluptatem ipsam exercitationem corrupti ea iusto culpa odio!
           Et ab recusandae possimus nobis dolor? Iste, totam. Est fugit ab atque itaque minus, omnis nam quos cum. Necessitatibus velit eaque harum impedit saepe ex maiores! Aperiam amet doloribus voluptatem.
@@ -35,7 +38,7 @@ const MentionsLegales = () => {
           Incidunt exercitationem expedita sunt itaque? Modi neque repellendus reiciendis maiores a consequatur. Debitis, accusantium. Enim officiis nisi beatae explicabo quis? Eius tempore sapiente ut iure voluptatibus ratione et vero voluptatem.
           Et labore molestias autem nisi consectetur nihil officiis voluptate quisquam, harum, quibusdam quis aliquam tenetur fuga itatibus.
         </p>
-      </article>
+      </article>      
     </section>
   );
 };
