4db3eed48c20ddedf1416eb4c441ca019abb7c95
Alex-Oz-157aws <maspimbya@gmail.com>
2021-03-24 12:17:10 +0100

    menu-burger

--- Front/src/components/Home/index.js
+++ Front/src/components/Home/index.js
@@ -3,12 +3,15 @@ import React from 'react';
 
 // Import local
 import NavBar from '../NavBar';
+import Burger from 'src/components/Burger';
+
 import Carrousel from '../Carrousel';
 
 const Home = () => {
   return (
     <div className="Home">
       <NavBar />
+      <Burger />
       <Carrousel />
       <Carrousel />
     </div>
