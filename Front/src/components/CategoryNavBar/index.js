a42a35d8d2b65f8db3cfc6d57ecd8a465490d1f7
Laurent Dashwood <laurentdashwood@gmail.com>
2021-03-24 19:02:05 +0100

    responsiveFluideDone

--- Front/src/components/CategoryNavBar/index.js
+++ Front/src/components/CategoryNavBar/index.js
@@ -1,16 +1,39 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
+// eslint-disable-next-line arrow-body-style
 const CategoryNavBar = () => {
-    return (
-      <nav className="NavBarCategory">
-        <ul className="NavBarCategory__link-list">
-          <li className="NavBarCategory__link NavBarCategory__link--1">High Tech</li>
-          <li className="NavBarCategory__link NavBarCategory__link--2">Electroménager</li>
-          <li className="NavBarCategory__link NavBarCategory__link--3">Alimentaire</li>
-          <li className="NavBarCategory__link NavBarCategory__link--4">Décoration</li>
-          <li className="NavBarCategory__link NavBarCategory__link--5">Habillement</li>
-        </ul>
-      </nav>
-    )
-}
+  return (
+    <nav className="NavBarCategory">
+      <ul className="NavBarCategory__link-list">
+        <li className="NavBarCategory__link NavBarCategory__link--1">
+          <NavLink to="/high_tech">
+            High Tech
+          </NavLink>
+        </li>
+        <li className="NavBarCategory__link NavBarCategory__link--2">
+          <NavLink to="/electromenager">
+            Electroménager
+          </NavLink>
+        </li>
+        <li className="NavBarCategory__link NavBarCategory__link--3">
+          <NavLink to="/alimentaire">
+            Alimentaire
+          </NavLink>
+        </li>
+        <li className="NavBarCategory__link NavBarCategory__link--4">
+          <NavLink to="/decoration">
+            Décoration
+          </NavLink>
+        </li>
+        <li className="NavBarCategory__link NavBarCategory__link--5">
+          <NavLink to="/habillement">
+            Habillement
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+
+  );
+};
 export default CategoryNavBar;
--- Front/src/components/PageCategory/index.js
+++ Front/src/components/PageCategory/index.js
@@ -3,28 +3,30 @@ import React from 'react';
 
 // Import local
 import CategoryNavBar from '../CategoryNavBar';
-import SubCategoriesImgs from '../SubCategoriesImgs';    
-import picto from '../../assets/images/shopping-cart.svg';     
+import SubCategoriesImgs from '../SubCategoriesImgs';
+import mainImg from '../../assets/temp/vegetables.jpg';
+
 
 const PageCategory = () => {
   return (
     <React.Fragment>
       <section className="CategoryPage">
         <CategoryNavBar />
-        <div className="MainImgCategory"></div>
-          <nav className="Pictogram">
-            <ul className="Pictogram__sub-list">
-              <li className="Pictogram__sub Pictogram__sub-1"><img src={picto} alt="picto" /></li>
-              <li className="Pictogram__sub Pictogram__sub-2"><img src="#" alt="picto" /></li>
-              <li className="Pictogram__sub Pictogram__sub-3"><img src="#" alt="picto" /></li>
-              <li className="Pictogram__sub Pictogram__sub-4"><img src="#" alt="picto" /></li>
-              <li className="Pictogram__sub Pictogram__sub-4"><img src="#" alt="picto" /></li>
-            </ul>
-          </nav>
-            <SubCategoriesImgs />
+        <div className="MainImgCategory">
+          <img className="MainImgCategory__img" src={mainImg} />
+        </div>
+        <nav className="Pictogram">
+          <ul className="Pictogram__sub-list">
+            <li className="Pictogram__sub Pictogram__sub-1"><img src="#" alt="picto" /></li>
+            <li className="Pictogram__sub Pictogram__sub-2"><img src="#" alt="picto" /></li>
+            <li className="Pictogram__sub Pictogram__sub-3"><img src="#" alt="picto" /></li>
+            <li className="Pictogram__sub Pictogram__sub-4"><img src="#" alt="picto" /></li>
+            <li className="Pictogram__sub Pictogram__sub-4"><img src="#" alt="picto" /></li>
+          </ul>
+        </nav>
+        <SubCategoriesImgs />
       </section>
     </React.Fragment>
   );
 };
-
-export default PageCategory
+export default PageCategory;
