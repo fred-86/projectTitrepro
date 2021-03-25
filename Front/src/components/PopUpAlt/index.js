cec68eb632eaf89b20bad872bc867e4da8903907
Alex-Oz-157aws <maspimbya@gmail.com>
2021-03-25 12:29:55 +0100

    popUpAlt-design

--- Front/src/components/PopUpAlt/index.js
+++ Front/src/components/PopUpAlt/index.js
@@ -14,17 +14,19 @@ const PopUpAlt = () => (
         <p className="description__text">
         Maecenas sit amet tincidunt lacus, sit amet fermentum velit. Mauris fermentum massa rhoncus rhoncus sagittis. Integer consequat, purus vitae viverra elementum, enim nulla egestas erat, a placerat odio quam et nulla. Donec bibendum mauris eget nulla egestas rhoncus. <br/>Praesent viverra efficitur tincidunt. Curabitur eget ipsum gravida, pulvinar ante sit amet, laoreet turpis. Pellentesque mauris purus, cursus a neque eu, rhoncus convallis tortor. Sed auctor nisl erat, sit amet pharetra arcu dictum in.
         </p>
+        <button className="description__link">Voir le site</button>
       </section>
       <aside className="info">
-        <h2 className="info__title">Adresse</h2>
+        <h2 className="info__address">Adresse</h2>
           <p className="info__text">15 rue de la Folie-Méricourt</p>
           <p className="info__text">75011 PARIS</p>
         <img className="info__map"></img>
       </aside>
     </div>
-    <div className="PopUpAlt__comment">
-      <h2 className="PopUpAlt__comment-title">Comment</h2>
-
+    <hr className="popUpAlt__hr"/>
+    <div className="popUpAlt__comment">
+      <h2 className="popUpAlt__comment-title">Comment</h2>
+      <input className="popUpAlt__input" placeholder="Votre avis nous intéresse !" />
     </div>
 
   </div>
