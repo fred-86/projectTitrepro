be06237a52ad3189e38b20105f8661427b7d4e69
Alex-Oz-157aws <maspimbya@gmail.com>
2021-03-25 11:08:05 +0100

    popUpAlt-canva

---
+++ Front/src/components/PopUpAlt/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const PopUpAlt = () => (
+  <iframe className="popUpAlt__frame">
+    <button className="popUpAlt__button">
+      X
+    </button>
+    <figure className="popUpAlt__picture">
+      <img>sdqsdqsdsdsqds</img>
+    </figure>
+    <h1 className="PopUpAlt__title">Nom Alternative</h1>
+
+    <div className="popUpAlt__content">
+      <section className="description">
+        <p className="description__text">sdsqdqdd</p>
+      </section>
+      <aside className="info">
+        <h2 className="info__title">Adresse</h2>
+        <p className="info__text">contenu adresse</p>
+        <img>sdqsdsdqsd</img>
+      </aside>
+    </div>
+    <div className="PopUpAlt__comment">
+      <h2 className="PopUpAlt__comment-title">Comment</h2>
+      <input type="Text">
+        dqsdqsdsqdqsdqsd
+      </input>
+    </div>
+
+  </iframe>
+);
+
+export default PopUpAlt;
