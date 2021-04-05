03fa096348883a25e0251f4c372e42ec530ca5ce
jfuzeau <jfuzeau@hotmail.fr>
2021-04-05 15:02:39 +0200

    responsive banner altHome

--- Front/src/components/AltHome/index.js
+++ Front/src/components/AltHome/index.js
@@ -7,7 +7,7 @@ import AltCategoryNavBar from '../AltCategoryNavBar';
 import AltCategoryCards from '../AltCategoryCards';
 import bannerImg from '../../assets/temp/forest.jpg';
 
-const AltHome = ({ token, loadPlaceCategories, placeCategories, loadPlaces, places, selectedPlace, setSelectedPlace, mainSwitch }) => {
+const AltHome = ({ token, loadPlaceCategories, placeCategories, loadPlaces, places, selectedPlace, setSelectedPlace }) => {
   const { id } = useParams();
   const placesByCategory = parseInt(id) === 0 ? places : places.filter((place) => (place.placeCategory.id === parseInt(id)));
 
@@ -24,25 +24,24 @@ const AltHome = ({ token, loadPlaceCategories, placeCategories, loadPlaces, plac
         <div className="AltHome__banner">
           <img className="AltHome__banner-img" src={bannerImg} alt="forest" />
           <article className="AltHome__banner-article">
-            <h1 className="AltHome__banner-title"> Bienvenue sur<br/>E-Pas-Commerce</h1>
+            <h1 className="AltHome__banner-title"> Bienvenue sur<br />E-Pas-Commerce</h1>
             <p className="AltHome__banner-text">
-            Supris(e) ? C’est normal.<br/>
+              Supris(e) ? C’est normal.<br />
             </p>
             <p className="AltHome__banner-text">
-            En arrivant ici, vous découvrez le but véritable de notre plate-forme.<br/> 
-            Nous n’avons pas vocation à vous empêcher de consommer.
+              En arrivant ici, vous découvrez le but véritable de notre plate-forme.<br />
+              Nous n’avons pas vocation à vous empêcher de consommer.
             </p>
             <p className="AltHome__banner-text">
-            Avec E-PaKo (contraction d’E-Pas-Commerce),  nous vous proposons un ensemble de solutions pour changer certaines habitudes de consommation héritées de notre quotidien ultra connecté.
-            </p>
-            <p className="AltHome__banner-text"> 
-            N’achetez pas tout de suite !<br/>
-            Découvrez plutôt de nouvelles alternatives proches de chez vous ET n’hésitez pas à nous faire part de vos trouvailles ou bon plans.
+              Avec E-PaKo (contraction d’E-Pas-Commerce),  nous vous proposons un ensemble de solutions pour changer certaines habitudes de consommation héritées de notre quotidien ultra connecté.
             </p>
             <p className="AltHome__banner-text">
-            Bonne visite ou à très bientôt ! 
-
-          </p>
+              N’achetez pas tout de suite !<br />
+              Découvrez plutôt de nouvelles alternatives proches de chez vous ET n’hésitez pas à nous faire part de vos trouvailles ou bon plans.
+            </p>
+            <p className="AltHome__banner-text">
+              Bonne visite ou à très bientôt !
+            </p>
           </article>
         </div>
         <AltCategoryNavBar placeCategories={placeCategories} />
