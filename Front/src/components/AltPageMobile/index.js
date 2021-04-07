3c122897311cefedec31d23ed75c285da00a3491
Alex-Oz-157aws <maspimbya@gmail.com>
2021-04-07 15:12:16 +0200

    update-altPageMobile

--- Front/src/components/AltPageMobile/index.js
+++ Front/src/components/AltPageMobile/index.js
@@ -20,12 +20,14 @@ const AltPageMobile = ({ token, loadPlaces, places, getMap, map }) => {
     <section className="AltPageMobile">
       <div className="Alt__banner">
         <img className="Alt__bannerImg" src={bannerAltCardImg} alt="Dons" />
-        <Link to="/practical/category/0" className="Alt__bannerBtn">
-          &#11178;
-        </Link>
       </div>
       <div className="Alt__presentation">
         {typeof currentPlace !== 'undefined' && <div className="Alt__wrapper">
+          <div className="Alt__returnToggle">
+            <Link to="/practical/category/0" className="Alt__returnBtn">
+            &#10554;
+            </Link>
+          </div>
           <h1 className="Alt__title">{currentPlace.name}</h1>
           <p className="Alt__description">{currentPlace.content}</p>
           <article className="Alt__adress">
@@ -35,7 +37,7 @@ const AltPageMobile = ({ token, loadPlaces, places, getMap, map }) => {
           </article>
           <div className="Alt__map">
             <img className="Alt__map--geoloc" src={map}></img>
-            <a className="Alt__map--btn" href={currentPlace.url} target="_blank">Visiter le site</a>
+            <button className="Alt__map--btn" href={currentPlace.url} target="_blank">Voir le site</button>
           </div>
           <div className="Alt__comment">
             <h2 className="Alt__comment--title">Commentaire</h2>
