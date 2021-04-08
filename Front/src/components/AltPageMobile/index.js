05615ba4c06f49802802b6381df652232eb821e6
Alex-Oz-157aws <maspimbya@gmail.com>
2021-04-08 14:28:15 +0200

    update-altBanner-ok

--- Front/src/components/AltPageMobile/index.js
+++ Front/src/components/AltPageMobile/index.js
@@ -24,7 +24,14 @@ const AltPageMobile = ({ token, loadPlaces, places, getMap, map }) => {
     <section className="AltPageMobile">
       <div className="Alt__banner">
         <div className="Alt__bannerQuote"  alt="Dons">
-          <p className="Alt__bannerQuote-text">sdqsdsqdsd</p>
+          <p className="Alt__bannerQuote-text">
+            “Le problème, ce n’est pas le fait qu’on aille dans le mur ou pas. On va dans le mur !<br/>
+            Le problème, c’est à quelle vitesse on y va, à 50 km/h ou à 5 km/h.”
+          </p>
+          <p className="Alt__bannerQuote-signature">
+            Jean-Marc Jancovici<br/>
+            Associé fondateur de Carbone 4
+          </p>
         </div>  
       </div>
       <div className="Alt__presentation">
