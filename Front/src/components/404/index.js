0af5797aeedac2eb7c13cf61c54dd5b7fc0dd4a4
jfuzeau <jfuzeau@hotmail.fr>
2021-04-13 13:27:51 +0200

    404 rework..

--- Front/src/components/404/index.js
+++ Front/src/components/404/index.js
@@ -1,11 +1,18 @@
 // Import npm
 import React from 'react';
+import { Link } from 'react-router-dom';
+import wallpaper from 'src/assets/images/404.png';
 
 const Page404 = () => (
   <div className="Page404">
-    <p className="Page404__content">
-      404
-    </p>
+    <img src={wallpaper} className="Page404__image" />
+    <article className="Page404__content">
+      <h1 classsName="Page404__content-title"> 404 </h1>
+      <p className="Page404__content-text">Oups,  Vous vous êtes perdu ! La page que vous cherchez n’a pas pu être trouvé </p>
+      <Link to="/" className="Page404__content-button">
+        Go back
+      </Link>
+    </article>
   </div>
 );
 
